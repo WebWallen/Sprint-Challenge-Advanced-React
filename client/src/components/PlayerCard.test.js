3985204f9764b8345c88c4f9d6b493b869442ec4
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-09-06 14:11:50 -0400

    Confused about the test results (still getting an undefined even though it shows the info correctly) but am not gonna spend anymore time on this as I still need to style and go to lunch.

--- client/src/components/PlayerCard.test.js
+++ client/src/components/PlayerCard.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import * as rtl from '@testing-library/react';
 import { render } from '@testing-library/react';
+import App from "../App";
 import PlayerCard from "./PlayerCard";
 
 test('It renders without crashing', () => {
-    render(<PlayerCard />);
+    render(<App />);
+})
+
+test('Search volume successfully pulled from API', () => {
+    const wrapper = render(<PlayerCard />);
+    const hasSearchResults = wrapper.getByText(/searches/i)
+    expect(hasSearchResults).toBeInTheDocument();
 })
